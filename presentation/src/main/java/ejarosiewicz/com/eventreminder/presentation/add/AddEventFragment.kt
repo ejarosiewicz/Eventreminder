32c4dedadf9e7487045bdc767d2db0f0dfdd1d24
Emil Jarosiewicz <emiljarosiewicz@gmail.com>
2019-01-09 20:28:01 +0100

    Initial work

--- presentation/src/main/java/ejarosiewicz/com/eventreminder/presentation/add/AddEventFragment.kt
+++ presentation/src/main/java/ejarosiewicz/com/eventreminder/presentation/add/AddEventFragment.kt
@@ -2,13 +2,21 @@ package ejarosiewicz.com.eventreminder.presentation.add
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ejarosiewicz.com.eventreminder.presentation.R
 
-class AddEventActivity: AppCompatActivity() {
+class AddEventFragment: Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_add, container, false)
     }
-
 
 }
--- presentation/src/main/java/ejarosiewicz/com/eventreminder/presentation/main/MainFragment.kt
+++ presentation/src/main/java/ejarosiewicz/com/eventreminder/presentation/main/MainFragment.kt
@@ -1,2 +1,28 @@
 package ejarosiewicz.com.eventreminder.presentation.main
 
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
+import ejarosiewicz.com.eventreminder.presentation.R
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment: Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        addEvent.setOnClickListener{findNavController().navigate(R.id.add_event)}
+    }
+
+}
